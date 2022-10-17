3a8c13c7d15efea504cbb3ba4c07755d99d622e1
Ayush Sharma <65230224+spaces7@users.noreply.github.com>
2022-10-16 23:17:47 -0700

    Add files via upload

---
+++ js/todo.js
@@ -0,0 +1,24 @@
+$("ul").on("click", "li", function(){
+    $(this).toggleClass("select");
+})
+
+$("ul").on("click", " li span", function(){
+    $(this).parent().fadeOut(500, function(){
+        this.remove();
+    })
+    $(this).stopPropagation();
+})
+
+$("input[type='text']").keypress(function(event) {
+    if(event.which === 13) {
+        if(!($(this).val() === "")) {
+            var text = $(this).val();
+            $("ul").append("<li><span><i class='fas fa-trash'></i></span>" + text +"</li>");
+        }
+        $(this).val("");
+    }
+})
+
+$(".fa-feather-alt").click(function(){
+    $("input[type='text']").fadeToggle();
+})
